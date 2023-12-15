115beb57201056f5a84f1703fda35cca776d801b
Dzmitry Lahunouski <killroy192@gmail.com>
2023-12-15 18:08:49 +0000

    Feat/new readme (#6)
    * update readme
    
    * fix readme example

--- integration/main.test.js
+++ integration/main.test.js
@@ -1,9 +1,9 @@
 const { expect } = require("chai");
-const { main } = require("../index");
+const { solBundler } = require("../index");
 
 describe("main", () => {
   it("should deploy contracts based on config file", async () => {
-    const { ctx, deployedContracts } = await main(hre);
+    const { ctx, deployedContracts } = await solBundler(hre);
     expect(deployedContracts).to.eql(["TestLibrary", "TestContract"]);
     expect(ctx).to.have.property("TestContract");
     expect(ctx).to.have.property("TestLibrary");
