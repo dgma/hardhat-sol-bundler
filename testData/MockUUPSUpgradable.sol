89e620374b08f5c9535f7de9be7c8bdc58908e12
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2024-12-12 12:24:45 +0000

    safe bump core deps

--- testData/MockUUPSUpgradable.sol
+++ testData/MockUUPSUpgradable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.8.20 <0.9.0;
 
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
