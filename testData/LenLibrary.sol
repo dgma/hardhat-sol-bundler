e209be3440c265c04a0ca71f23db5357ade32137
Dzmitry Lahunouski <killroy192@gmail.com>
2024-12-12 12:43:23 +0000

    Feat/bump deps (#38)
    * safe bump core deps
    
    * bump solc
    
    * bump version

--- testData/LenLibrary.sol
+++ testData/LenLibrary.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.8.20 <0.9.0;
 
 library LenLibrary {
     function messageLen(string memory message) external pure returns (uint256) {
